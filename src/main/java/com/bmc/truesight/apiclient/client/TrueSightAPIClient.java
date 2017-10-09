8d339969b5a77a99a56cd0530ed182d1007edfd6
Mathew <jomathew@bmc.com>
2017-10-09 17:46:08 +0530

    Incorporated reivew commetns

--- src/main/java/com/bmc/truesight/apiclient/client/TrueSightAPIClient.java
+++ src/main/java/com/bmc/truesight/apiclient/client/TrueSightAPIClient.java
@@ -1,7 +1,18 @@
 /*
- * Copyright (c) 2017 bmc.com
- * All rights reserved.
- */
+Copyright 2017 BMC Software, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.bmc.truesight.apiclient.client;
 
 import com.bmc.truesight.apiclient.dto.Event;
@@ -24,60 +35,80 @@ import org.slf4j.LoggerFactory;
 public final class TrueSightAPIClient { 
     
     private static final Logger LOG = LoggerFactory.getLogger(TrueSightAPIClient.class);
-     
+    
+    private final TrueSightCredentials credentials;
+    private final int retryDelay;
+        
     /**
-     * addMeasures() invoking the API /v1/measurements.
+     * 
+     * This constructor will set the default retry delay to 30 seconds.
+     * @param creds Basic authentication credentials required for REST API call. 
+     */    
+    public TrueSightAPIClient(TrueSightCredentials creds)
+    {
+        this.credentials = creds;
+        this.retryDelay = 30000;
+    }
+    
+    /**
+     * 
+     * @param creds Basic authentication credentials required for REST API call.
+     * @param retryDelay Number of milliseconds the client has to wait during a retry.
+     */
+    
+    public TrueSightAPIClient(TrueSightCredentials creds,  int retryDelay)
+    {
+        this.credentials = creds;
+        this.retryDelay = retryDelay;
+    }
+    
+         
+    /**
+     * addMeasures() method invokes the API endpoint /v1/measurements.
      * returns a TrueSightClientResponse on successful execution.
      * The method takes a list of Measurement Objects (1 or more), a TrueSightCredentials object
      * and the sleep time in milliseconds that the system need to wait during a retry.
      * 
      * @param measures List of Measurement objects which need to be pushed to TrueSight.
-     * @param credentials   Basic authentication credentials required for REST API call.
-     * @param retrySleepTime Number of milliseconds the client has to wait during a retry.
      * @return TrueSightClientResponse  Provides HTTP status code and JSON response from the API.
      * @throws TrueSightClientException Wraps other exceptions thrown during the method execution.
      * @see TrueSightClientResponse
      * @see Measurement
      * @see TrueSightCredentials
      */
-    public TrueSightClientResponse addMeasures(List<Measurement> measures, TrueSightCredentials credentials, int retrySleepTime) throws TrueSightClientException {
+    public TrueSightClientResponse addMeasures(List<Measurement> measures) throws TrueSightClientException {
         
-        LOG.info("Inovking addMeasures() ...");
+        LOG.debug("Inovking addMeasures() ...");
         TrueSightClientResponse result = null;
         if(measures != null && measures.size() > 0) 
         {
             LOG.debug("getMeasures() called with {} Measurements", measures.size());
             List<Object> bulkMeasures = TrueSightAPIClientUtility.toBulkEntryMeasureFormat(measures);            
             String endpoint = TrueSightAPIClientUtility.getHost() + Configuration.MEASURE_ENDPOINT;
-            result = TrueSightAPIClientUtility.executePost(bulkMeasures, credentials, endpoint, retrySleepTime);
+            result = TrueSightAPIClientUtility.executePost(bulkMeasures, credentials, endpoint, retryDelay);
             LOG.debug("ClientResponse: {}", result.toString());
         }
-        else
-        {
-            LOG.error("Failed to add Measurement: Measurement list is null or empty");
-            throw new TrueSightClientException("Failed to add Measurement: Measurement list is null or empty");
-        }
+        else        
+            LOG.warn("Failed to add Measurement: Measurement list is null or empty");                    
         
         return result;
     }  
     
     
     /**
-     * addMeasures() invoking the API /v1/batch/metrics.
+     * createMetrics() method invokes the API endpoint /v1/batch/metrics.
      * returns a TrueSightClientResponse on successful execution.
      * The method takes a list of Metric Objects (1 or more), a TrueSightCredentials object
      * and the sleep time in milliseconds that the system need to wait during a retry.
      * 
      * @param metrics List of Metric objects which need to be pushed to TrueSight.
-     * @param credentials   Basic authentication credentials required for REST API call.
-     * @param retrySleepTime Number of milliseconds the client has to wait during a retry.
      * @return TrueSightClientResponse  Provides HTTP status code and JSON response from the API.
      * @throws TrueSightClientException Wraps other exceptions thrown during the method execution.
      * @see TrueSightClientResponse
      * @see Metric
      * @see TrueSightCredentials
      */
-    public TrueSightClientResponse createMetrics(List<Metric> metrics, TrueSightCredentials credentials, int retrySleepTime) throws TrueSightClientException {
+    public TrueSightClientResponse createMetrics(List<Metric> metrics) throws TrueSightClientException {
         
         TrueSightClientResponse result = null;
         if(metrics != null && metrics.size() > 0)
@@ -85,10 +116,10 @@ public final class TrueSightAPIClient {
             //Converting the List<Metrics> to List<Object>.
             List<Object> objectList = new ArrayList<>(metrics);            
             String endpoint = TrueSightAPIClientUtility.getHost() + Configuration.METRIC_ENDPOINT;
-            result = TrueSightAPIClientUtility.executePost(objectList, credentials, endpoint, retrySleepTime);
+            result = TrueSightAPIClientUtility.executePost(objectList, credentials, endpoint, retryDelay);
         } 
         else
-            throw new TrueSightClientException("Failed to add Metric: Metric list is null or empty");
+            LOG.warn("Failed to create Metrics: Metric list is null or empty");
                 
         return result;         
     }    
@@ -96,31 +127,29 @@ public final class TrueSightAPIClient {
     
     
     /**
-     * addMeasures() invoking the API /v1/events.
+     * createEvents() method invokes the API endpoint /v1/events.
      * returns a TrueSightClientResponse on successful execution.
      * The method takes a list of Event Objects (1 or more), a TrueSightCredentials object
      * and the sleep time in milliseconds that the system need to wait during a retry.
      * 
      * @param events List of Event objects which need to be pushed to TrueSight.
-     * @param credentials   Basic authentication credentials required for REST API call.
-     * @param retrySleepTime Number of milliseconds the client has to wait during a retry.
-     * @return TrueSightClientResponse  Provides HTTP status code and JSON response from the API.
+      * @return TrueSightClientResponse  Provides HTTP status code and JSON response from the API.
      * @throws TrueSightClientException Wraps other exceptions thrown during the method execution.
      * @see TrueSightClientResponse
      * @see Event
      * @see TrueSightCredentials
      */
-    public TrueSightClientResponse createEvents(List<Event> events, TrueSightCredentials credentials, int retrySleepTime) throws TrueSightClientException {
+    public TrueSightClientResponse createEvents(List<Event> events) throws TrueSightClientException {
         
         TrueSightClientResponse result = null;
         if(events != null && events.size() > 0)
         {
             List<Object> objectList = new ArrayList<>(events);
             String endpoint = TrueSightAPIClientUtility.getHost() + Configuration.EVENT_ENDPOINT;
-            result = TrueSightAPIClientUtility.executePost(objectList, credentials, endpoint, retrySleepTime);
+            result = TrueSightAPIClientUtility.executePost(objectList, credentials, endpoint, retryDelay);
         }
         else
-            throw new TrueSightClientException("Failed to create Event: Event list is null or empty");
+            LOG.warn("Failed to create Events: Event list is null or empty");                    
         return result;
     }
 }
--- src/main/java/com/bmc/truesight/apiclient/utility/TrueSightAPIClientUtility.java
+++ src/main/java/com/bmc/truesight/apiclient/utility/TrueSightAPIClientUtility.java
@@ -1,7 +1,18 @@
 /*
- * Copyright (c) 2017 bmc.com
- * All rights reserved.
- */
+Copyright 2017 BMC Software, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.bmc.truesight.apiclient.utility;
 
 import com.bmc.truesight.apiclient.dto.Measurement;
@@ -82,8 +93,8 @@ public class TrueSightAPIClientUtility {
         httpPost.addHeader("User-Agent", "TrueSightAPIClient");
         Charset charsetD = Charset.forName("UTF-8");
         
-        //Setting the minimum sleep time to 1000.
-        int interval = retrySleepTime > 1000 ? retrySleepTime: 1000;
+        //Setting the minimum sleep time to 30000.
+        int interval = retrySleepTime > 30000 ? retrySleepTime: 30000;
         
         int retry = 3;
         int retryCount = 0;
